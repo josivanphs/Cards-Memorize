0daee86df359f86948120b5719a16242beec65a5
josivanphs <jhosivan@outlook.com>
2022-07-26 21:23:15 -0400

    feat

---
+++ cardsMemorize/cardsMemorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cardsMemorize
+//
+//  Created by Mikael Fonseca on 25/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cardsMemorize/cardsMemorize/cardsMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsMemorizeApp.swift
+//  cardsMemorize
+//
+//  Created by Mikael Fonseca on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct cardsMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
